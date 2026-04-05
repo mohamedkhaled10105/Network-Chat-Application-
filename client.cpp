e174726c71fe803c08111e10edcc50cc91b26426
mohamedkhaled10105 <mohamedkhaled4343@gmail.com>
2026-04-05 12:32:53 +0000

    added continuous chat loop for real time messaging

--- client.cpp
+++ client.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
-#include <cstring>
-#include <arpa/inet.h>
-#include <sys/socket.h>
-#include <unistd.h>
+#include <cstring>          
+#include <arpa/inet.h>      
+#include <sys/socket.h>     
+#include <unistd.h>         
 
 #define PORT 8080
 using namespace std;
@@ -10,24 +10,37 @@ using namespace std;
 int main() {
     int sock;
     struct sockaddr_in serv_addr;
-    char buffer[1024] = {0};
+    char buffer[1024];
 
+    // Create socket (TCP)
     sock = socket(AF_INET, SOCK_STREAM, 0);
 
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
-    inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
+    // Setup server address
+    serv_addr.sin_family = AF_INET;        
+    serv_addr.sin_port = htons(PORT);      
+    inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr); 
 
+    // 3. Connect to server
     connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+    cout << "Connected to server!\n";
 
-    // Send message
-    char msg[] = "Hello from client";
-    send(sock, msg, strlen(msg), 0);
+    // Chat loop
+    while (true) {
+        // Send message
+        cout << "You: ";
+        cin.getline(buffer, sizeof(buffer));
+        send(sock, buffer, strlen(buffer), 0);
 
-    // Receive reply
-    recv(sock, buffer, sizeof(buffer), 0);
-    cout << "Server: " << buffer << endl;
+        // Receive reply
+        memset(buffer, 0, sizeof(buffer));
+        int bytes = recv(sock, buffer, sizeof(buffer), 0);
+        if (bytes <= 0) break;             
 
+        cout << "Server: " << buffer << endl;
+    }
+
+    // Close socket
     close(sock);
+
     return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
-#include <cstring>
-#include <unistd.h>
-#include <arpa/inet.h>
-#include <sys/socket.h>
+#include <cstring>          
+#include <unistd.h>         
+#include <arpa/inet.h>      
+#include <sys/socket.h>     
 
 #define PORT 8080
 using namespace std;
@@ -10,29 +10,44 @@ using namespace std;
 int main() {
     int server_fd, client_socket;
     struct sockaddr_in addr;
-    char buffer[1024] = {0};
+    char buffer[1024];
 
+    // Create socket (TCP)
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
 
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(PORT);
-    addr.sin_addr.s_addr = INADDR_ANY;
+    // Setup server address
+    addr.sin_family = AF_INET;            
+    addr.sin_port = htons(PORT);          
+    addr.sin_addr.s_addr = INADDR_ANY;    
 
+    // Bind socket to port
     bind(server_fd, (struct sockaddr*)&addr, sizeof(addr));
-    listen(server_fd, 3);
 
+    // Start listening for clients
+    listen(server_fd, 3);
     cout << "Server waiting...\n";
 
+    // Accept a client connection
     client_socket = accept(server_fd, NULL, NULL);
+    cout << "Client connected!\n";
 
-    // Receive message
-    recv(client_socket, buffer, sizeof(buffer), 0);
-    cout << "Client: " << buffer << endl;
+    // Chat loop (continuous communication)
+    while (true) {
+        memset(buffer, 0, sizeof(buffer));   
 
-    // Send reply
-    char reply[] = "Hello from server";
-    send(client_socket, reply, strlen(reply), 0);
+        // Receive message from client
+        int bytes = recv(client_socket, buffer, sizeof(buffer), 0);
+        if (bytes <= 0) break;               
 
+        cout << "Client: " << buffer << endl;
+
+        // Send reply to client
+        cout << "You: ";
+        cin.getline(buffer, sizeof(buffer));
+        send(client_socket, buffer, strlen(buffer), 0);
+    }
+
+    // Close sockets
     close(client_socket);
     close(server_fd);
 

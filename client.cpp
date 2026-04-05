5bf4ebb5e279f237213fd2c8fba552a98b527929
mohamedkhaled10105 <mohamedkhaled4343@gmail.com>
2026-04-05 12:23:09 +0000

    implemented single message exchange between client and server

--- client.cpp
+++ client.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <unistd.h>
@@ -9,19 +10,23 @@ using namespace std;
 int main() {
     int sock;
     struct sockaddr_in serv_addr;
+    char buffer[1024] = {0};
 
-    // Create socket
     sock = socket(AF_INET, SOCK_STREAM, 0);
 
-    // Setup server address
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
     inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
 
-    // Connect to server
     connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
 
-    cout << "Connected to server!\n";
+    // Send message
+    char msg[] = "Hello from client";
+    send(sock, msg, strlen(msg), 0);
+
+    // Receive reply
+    recv(sock, buffer, sizeof(buffer), 0);
+    cout << "Server: " << buffer << endl;
 
     close(sock);
     return 0;
--- server.cpp
+++ server.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
@@ -7,26 +8,30 @@
 using namespace std;
 
 int main() {
-    int server_fd;
+    int server_fd, client_socket;
     struct sockaddr_in addr;
+    char buffer[1024] = {0};
 
-    // Create socket
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
 
-    // Setup address
     addr.sin_family = AF_INET;
     addr.sin_port = htons(PORT);
     addr.sin_addr.s_addr = INADDR_ANY;
 
-    // Bind and listen
     bind(server_fd, (struct sockaddr*)&addr, sizeof(addr));
     listen(server_fd, 3);
 
-    cout << "Server waiting on port 8080...\n";
+    cout << "Server waiting...\n";
 
-    // Accept client
-    int client_socket = accept(server_fd, NULL, NULL);
-    cout << "Client connected!\n";
+    client_socket = accept(server_fd, NULL, NULL);
+
+    // Receive message
+    recv(client_socket, buffer, sizeof(buffer), 0);
+    cout << "Client: " << buffer << endl;
+
+    // Send reply
+    char reply[] = "Hello from server";
+    send(client_socket, reply, strlen(reply), 0);
 
     close(client_socket);
     close(server_fd);
